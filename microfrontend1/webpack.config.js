9e5f4f04983a337e36d8f65667f92a22bd643680
Your Name <you@example.com>
2022-01-06 15:23:08 +0100

    primera aproximación microfrontend

---
+++ microfrontend1/webpack.config.js
@@ -0,0 +1,73 @@
+const { ModuleFederationPlugin } = require("webpack").container;
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ExternalTemplateRemotesPlugin = require("external-remotes-plugin");
+const path = require("path");
+const ReactRefreshWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin");
+const libsUrl = "http://localhost:3002";
+
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+
+module.exports = {
+  entry: "./src/index.tsx",
+  mode: "development",
+  devtool: "source-map",
+  optimization: {
+    minimize: false,
+  },
+  devServer: {
+    hot: true,
+    static: path.join(__dirname, "dist"),
+    port: 3001,
+    liveReload: false,
+    headers: {
+        'Access-Control-Allow-Origin': '*'
+    }
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".jsx", ".js", ".json", ".css"],
+    plugins: [new TsconfigPathsPlugin()]
+  },
+  output: {
+    publicPath: "auto",
+    clean: true,
+    path: path.join(__dirname, "dist"),
+    filename: 'static/js/[name].[chunkhash:8].js',
+    chunkFilename: 'static/js/[name].[chunkhash:8].chunk.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        loader: "babel-loader",
+        exclude: /node_modules/,
+        options: {
+          presets: ["@babel/preset-react"], 
+          plugins: [require.resolve("react-refresh/babel")],
+        },
+      },
+      {
+        test: /\.(ts|tsx)?$/,
+        loader: 'ts-loader',
+        exclude: /node_modules/,
+        options: {
+          configFile: "tsconfig.json"
+        }
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new ModuleFederationPlugin({}),
+    new ExternalTemplateRemotesPlugin(),
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+      chunks: ["main"],
+    }),
+    new ReactRefreshWebpackPlugin({
+      exclude: [/node_modules/, /bootstrap\.js$/],
+    })
+  ]
+};
