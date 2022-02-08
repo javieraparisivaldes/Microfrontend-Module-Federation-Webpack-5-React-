56f41f5d334cf8d7aae89c36e83c2db7df50205f
Francisco Javier Aparisi Valdés <francisco.aparisi@babelgroup.com>
2022-02-08 11:39:53 +0100

    delete microfront broke

--- microfrontend1Broke/src/Page2.js
+++
@@ -1,7 +0,0 @@
-import React from "libs/react";
-
-const Page2 = () => {
-  return <h1>This is Page2</h1>;
-};
-
-export default Page2;
--- microfrontend1Broke/src/declaration.d.ts
+++
@@ -1,3 +0,0 @@
-declare module 'libs/react';
-declare module 'libs/react-dom';
-declare module 'libs/react-router-dom'
